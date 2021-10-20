4ea9148915102b87e309145cfd762e6ad7c96ef6
Esmaelhbr <esmael7@live.com>
2021-10-20 14:57:33 -0400

    add index js and package json

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Card from './Card';
 import 'tachyons';
 import {robots} from "./robots"
 
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <div>
@@ -23,4 +23,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
