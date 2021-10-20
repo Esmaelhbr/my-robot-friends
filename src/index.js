d6a6beacf3b55b8689b4a0005555e7f8df379d3f
Esmaelhbr <esmael7@live.com>
2021-10-20 14:24:13 -0400

    added card components and robot data

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
+import 'tachyons';
+import {robots} from "./robots"
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+  	<Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+  	<Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+  	<Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+  	<Card id={robots[3].id} name={robots[3].name} email={robots[3].email} />
+  	
+  </div>
+  
+  ,
   document.getElementById('root')
 );
 
