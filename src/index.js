4c7eccaea470578b4a9b41b4fd56cea64fa29485
Esmaelhbr <esmael7@live.com>
2021-10-20 22:33:56 -0400

    be able to display all the robots from a file

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import CardList from './CardList';
 import 'tachyons';
 import {robots} from "./robots"
 
 
 
-ReactDOM.render(
-  <div>
-  	<Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-  	<Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-  	<Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-  	<Card id={robots[3].id} name={robots[3].name} email={robots[3].email} />
-  	
-  </div>
-  
+ReactDOM.render(<CardList robots={robots}/>
   ,
   document.getElementById('root')
 );
