62b5d03bbfe3a2eddee085bd94c31d41fdfcf45a
Esmaelhbr <esmael7@live.com>
2021-10-22 21:39:31 -0400

    Organized the files in folders

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
-import {robots} from "./robots";
-import SearchBox from "./SearchBox";
-import Scroll from "./Scroll";
+import CardList from '../components/CardList';
+import {robots} from "../robots";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
 import './App.css'
 
 
